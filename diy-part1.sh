0ad3fec276b27a2217820c0111da1278d1edf363
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2024-02-12 16:54:31 +0800

    Replace /lib/netifd/ppp-down

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,16 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-#按照mtk-sdk和最新openwrt主线的有线驱动，mt7981的ADMAv2存在不可修复的问题，immortalwrt-mt798x已参照上述源码将mt7981 ADMAv2回退至ADMAv1。
-#当有线驱动使用ADMAv1时，每个PPE最多支持16384个Entry。每个NAT连接需要占用2个Entry（进站和出站方向）。
-sed -i 's/nf_conntrack_max=65536/nf_conntrack_max=16384/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
-sed -i 's/nf_conntrack_buckets=65536/nf_conntrack_buckets=16384/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+# Replace /lib/netifd/ppp-down
+# https://github.com/hanwckf/immortalwrt-mt798x/issues/57
+cat >> package/network/services/ppp/files/lib/netifd/ppp-down <<EOF
+while true; do
+  if ubus -S list "network.interface.wan_6"; then
+    # ubus call network.interface.wan_6 down
+    logger "waiting wan_6 down..."
+    sleep 1
+  else
+    break
+  fi
+done
+EOF
